ed2d840f7d321e684748af6bf8f91b3029a6cb71
jinhee oh <exojj.com.gmail.com>
2022-02-19 16:24:08 +0900

    3rd add

--- src/App.js
+++ src/App.js
@@ -15,36 +15,45 @@ function App() {
   let [list_input, setlist_input] = useState('');
   let [swich, setswich] = useState(false);
   let [count, setcount] = useState(1);
-  let [mapIndex, setmapIndex] = useState(0);
+  let mapIndex = 0;   // useState()로 변경 하면 조금 늦게 처리되어 수정 에러가 난다.
+
+  let today = new Date();
+  let year = today.getFullYear(); 
+  let month = today.getMonth() + 1;
+  let saveDay = today.getDate();
+ // console.log(saveDay );     
 
   let item_obj = {
                id : count,
                contant : list_input,
-               isON : false
-           }; 
+               isON : false,
+               Day : [year, month, saveDay],
+           };     
  
   let save = () => {  
-        let copy = [...ya_list, item_obj];    
-       console.log(copy);     //여기는 추가가 됬는데 ...
+        let copy = [...ya_list, item_obj];   
         localStorage.setItem('list', JSON.stringify(copy)); 
-        setya_list(copy);     //왜 stata에 추가가 안되지?....
+        setya_list(copy);     
         setlist_input('');    
         // setcount(count + 1);
-       console.log(ya_list);
+    //    console.log(ya_list);
     }
 
   let changeON = () => {     
       let copy = [...ya_list];              
       copy[mapIndex]['isON'] = !copy[mapIndex]['isON'];
-      console.log(copy);  
+   //   console.log(copy);  
       localStorage.setItem('list', JSON.stringify(copy)); 
       setya_list(copy);   //여기는 되는데 ....
-      console.log(ya_list);   
+   //   console.log(ya_list);   
    }  
 
-  // let delete = () => { 
-
-  // }
+  let Nomal_Delete = () => { 
+      let copy = [...ya_list];   
+      copy.splice(mapIndex, 1);    
+      localStorage.setItem('list', JSON.stringify(copy)); 
+      setya_list(copy); 
+  }
 
   return (
     <div className="App">
@@ -60,16 +69,22 @@ function App() {
         </Nav.Item>        
       </Nav> 
       <div id="ya_main">
-        <h4>야 나두써</h4> <button onClick={() => { localStorage.removeItem('list'); setya_list([]);}} >전체삭제</button>
+        <h4>야 나두써  <button onClick={() => { 
+                localStorage.removeItem('list'); 
+                alert('정말로 전부 삭제하시죠?'); 
+                setya_list([]);}} >전체삭제</button></h4> 
         <div id="ya_list">
           {  
              ya_list.length > 0 
-             ? ya_list.map((item, i) => {
+             ? ya_list.map((item, i) => {                
                     return (
                         <div key={i} className={`listbox listbox${item.id}`} >
-                          <input type="checkbox" className='ck' onChange={() => { setmapIndex(i); changeON(); }}/>
-                          <p className={item.isON ? 'on' : ''}>{item.contant}</p>
-                          <button >X</button>
+                          <input type="checkbox" checked={item.isON} className='ck' onChange={() => { mapIndex = i; changeON(); }}/>
+                          <article>
+                            <p className={item.isON ? 'on' : ''}>{item.contant}</p>
+                            <h6>{item.Day[0]}년 {item.Day[1]}월 {item.Day[2]}일</h6>
+                          </article>
+                          <button onClick={() => { mapIndex = i; Nomal_Delete(); }}>X</button>
                         </div>
                     );
                 })             
